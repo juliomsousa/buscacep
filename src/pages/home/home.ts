4f68efa580202375c5bad416a1e0615895e2e174
Julio Martins <juliomartinssousa@gmail.com>
2017-02-05 13:07:57 -0200

    primeira versão funcional

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,35 +10,28 @@ import { ViaCepData } from '../../providers/via-cep-data';
 })
 export class HomePage {
 
-  public endereco: Endereco;
-  public cep = '05210070';
-
-  constructor(public navCtrl: NavController, public viaCepService: ViaCepData) {
-      //console.log(this.endereco);
-      //this.ionViewDidLoad();
-  }
-
-  ngOnInit() {
-      
-  }
-
-  ionViewDidLoad() {
-      
-      this.viaCepService.getCep(this.cep).subscribe(data => {
-          this.endereco = data;
-      });
-  }
-
+    public endereco: Endereco;
+  
+    constructor(public navCtrl: NavController, public viaCepService: ViaCepData) {
+  
+    }
+  
+    buscaCep(cep) {
+        this.viaCepService.getCep(cep).subscribe(data => {
+            this.endereco = data;
+        });
+    }
+    
 }
 
 interface Endereco {
-  cep: string;
-  logradouro: string;
-  complemento: string;
-  bairro: string;
-  localidade: string;
-  uf: string;
-  unidade: string;
-  ibge: string;
-  gia: string;
+    cep: string;
+    logradouro: string;
+    complemento: string;
+    bairro: string;
+    localidade: string;
+    uf: string;
+    unidade: string;
+    ibge: string;
+    gia: string;
 }
